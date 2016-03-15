f76696ff210543b4b5ac672665aaa2b723c290a0
serialworm <eric.beringer@gmail.com>
2016-03-15 14:42:14 -0700

    REST api tests, documentation and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # python-rest-example
+
+This example contains a feed class to retrieve and parse the html from the Metacritic ps3 page and a REST api component to access the data.
+## Setup
+```shell
+$ pip install .
+```
+
+## Running the tests for feed / api
+```shell
+$ python -m unittest discover
+```
+## Running the REST api
+
+```shell
+$ python app.py
+```
+
+Access it in a browser at:
+
+http://localhost:5000/games
+
+http://localhost:5000/Yakuza%205
---
+++ test/test_api.py
@@ -0,0 +1,41 @@
+from app import app
+import unittest
+from flask import json
+
+
+class TestApi(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+        res = self.app.get('/games')
+        items = json.loads(res.data)
+        # Set the first item in the response for testing the second endpoint
+        self.single_item = items.pop()
+
+    def tearDown(self):
+        pass
+
+    def test_games_status_code(self):
+        res = self.app.get('/games')
+        self.assertEqual(res.status_code, 200)
+
+    def test_games_data(self):
+        res = self.app.get('/games')
+        self.assertTrue(res.data)
+
+    def test_game_status_code(self):
+        res = self.app.get('/games/' + self.single_item['title'])
+        self.assertEqual(res.status_code, 200)
+
+    def test_game_data(self):
+        res = self.app.get('/games/' + self.single_item['title'])
+        self.assertTrue(res.data)
